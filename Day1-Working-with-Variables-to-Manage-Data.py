f724f4d2edb94285b4d957c9817fe49910aa722b
Shyam Patel <Shyam.Patel.MIS@gmail.com>
2021-01-04 12:51:40 -0600

    Day 1 Project Commited

---
+++ Day1-Working-with-Variables-to-Manage-Data.py
@@ -0,0 +1,12 @@
+#1. Create a greeting for your program.
+print("Hello this is my day 1 program of 100 days of Python.\n")
+
+#2. Ask the user for the city that they grew up in.
+userCity = input("Would you mind telling me what city you grew up in?\n")
+
+#3. Ask the user for the name of a pet.
+userPet = input("Would you mind telling me the name of a pet you have?\n")
+
+#4. Combine the name of their city and pet and show them their band name
+userBandName = userPet + userCity
+print("Your band name is... " + userBandName + "\n")
